ca9f7ea433e23cdbbfb2359fc9170b441c58a5bf
Berfin SAYILIR <89409721+berfinsayilir@users.noreply.github.com>
2021-10-21 12:42:26 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text = Convert.ToString(x * 9 / 5 + 32) + " F";
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double y = Convert.ToDouble(textBox1.Text);
+
+            textBox2.Text = Convert.ToString((y - 32) * 5 / 9)+ " C";
+            
+        }
+    }
+}
